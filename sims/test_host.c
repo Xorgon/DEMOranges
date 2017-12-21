829ecdc8467f9f2af93951870b102183573458c8
Xorgon <norman.reggox@gmail.com>
2017-12-21 23:30:40 +0000

    Implemented simple simulation run.

--- sims/test_host.c
+++ sims/test_host.c
@@ -16,7 +16,12 @@
 
 particle *hparticles;
 cl_mem gparticles;
-cl_ulong NUMPART = 10;
+cl_ulong NUMPART = 1000;
+
+cl_float timestep = 5e-4;
+cl_float sim_length = 10;
+cl_float last_write = 0;
+cl_float log_step = 0.1;
 
 cl_platform_id *platforms;
 cl_device_id *devices;
@@ -36,26 +41,31 @@ int main() {
     cl_float fluid_viscosity = 0.0000193;
 
     for (cl_ulong i = 0; i < NUMPART; i++) {
-        hparticles[i].particle_id = i;
+        hparticles[i].id = i;
         hparticles[i].density = density;
         hparticles[i].fluid_viscosity = fluid_viscosity;
-        hparticles[i].particle_diameter = particle_diameter;
-        hparticles[i].pos = (cl_float3) {0.0, i, 0.0};
+        hparticles[i].diameter = particle_diameter;
+        hparticles[i].pos = (cl_float3) {0.0, i + 10, 0.0};
         hparticles[i].vel = (cl_float3) {0.0, 0.0, 0.0};
         hparticles[i].forces = (cl_float3) {0.0, 0.0, 0.0};
     }
 
+    writeParticles(hparticles, 0, "TEST", "", NUMPART);
+
     gparticles = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(particle) * NUMPART, NULL, &ret);
 
-    ret = particlesToDevice(queue, gparticles, &hparticles, NUMPART);
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), &gparticles);
-    ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, (size_t *) &NUMPART, 0, NULL, NULL, NULL);
-    ret = particlesToHost(queue, gparticles, &hparticles, NUMPART);
-    ret = clFinish(queue);
-
-    for (cl_ulong i = 0; i < NUMPART; i++) {
-        printf("%f\n", hparticles[i].pos.y);
-//        printParticle(&hparticles[i]);
+    for (cl_float time = timestep; time <= sim_length; time += timestep) {
+        ret = particlesToDevice(queue, gparticles, &hparticles, NUMPART);
+        ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), &gparticles);
+        ret = clSetKernelArg(kernel, 1, sizeof(cl_float), &timestep);
+        ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, (size_t *) &NUMPART, 0, NULL, NULL, NULL);
+        ret = particlesToHost(queue, gparticles, &hparticles, NUMPART);
+        ret = clFinish(queue);
+        if (time - last_write > log_step) {
+            printf("Logging at time: %f\n", time);
+            writeParticles(hparticles, time, "TEST", "", NUMPART);
+            last_write = time;
+        }
     }
 
 
