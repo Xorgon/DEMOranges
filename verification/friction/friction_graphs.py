9b032223f0b14ffcd56a836f99037ac3a47c3bc8
Xorgon <norman.reggox@gmail.com>
2018-03-05 18:44:33 +0000

    Added friction verification case and grapher.

---
+++ verification/friction/friction_graphs.py
@@ -0,0 +1,102 @@
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import re
+
+
+def calculate_damping_coefficient(stiffness, restitution, m1, m2):
+    ln_rest = log(restitution)
+    return -2 * ln_rest * (get_reduced_particle_mass(m1, m2) * stiffness / (pi ** 2 + ln_rest ** 2)) ** 0.5
+
+
+def get_mass(density, diameter):
+    return density * pi * diameter ** 3 / 6
+
+
+def get_reduced_particle_mass(m1, m2):
+    return m1 * m2 / (m1 + m2)
+
+
+def get_pos(time, mass, friction_coeff, u_0, g):
+    return - friction_coeff * mass * g * time ** 2 / 2 + u_0 * time
+
+
+def get_vel(time, mass, friction_coeff, u_0, g):
+    return - friction_coeff * mass * g * time + u_0
+
+
+def plot_pos_and_vel():
+    stiffness = 1e5
+    density = 2000
+    diameter = 0.1
+    mass = get_mass(density, diameter)
+    restitution = 0.8
+    friction_coeff = 0.6
+    g = 9.81
+    u_0 = 1
+
+    damping_coeff = calculate_damping_coefficient(stiffness, restitution, mass, mass)
+
+    col_duration = pi * sqrt(mass / stiffness)
+
+    # Generate Analytic Solution Data
+    t_max = u_0 / (friction_coeff * mass * g)  # From Velocity equation
+    print("t_max = " + str(t_max))
+    times = np.arange(0, t_max, col_duration / 16)
+    positions = []
+    velocities = []
+
+    for t in times:
+        positions.append(get_pos(t, mass, friction_coeff, u_0, g))
+        velocities.append(get_vel(t, mass, friction_coeff, u_0, g))
+    times = np.append(times, 2.5)
+    positions.append(positions[-1])
+    velocities.append(0)
+
+    # Get Simulation Data
+    data = []
+    increments = []
+    data_dir = os.listdir("data")
+    for filename in data_dir:
+        name_match = re.match("1_friction_(\d+)_\d+.txt", filename)
+        if name_match:
+            i = int(name_match.group(1))
+            if increments.count(i) == 0:
+                increments.append(i)
+    for i in increments:
+        timestep_data = np.array([]).reshape((0, 3))  # Times, Positions, Velocities
+        for filename in data_dir:
+            name_match = re.match("1_friction_" + str(i) + "_(\d+).txt", filename)
+            if name_match:
+                time = int(name_match.group(1)) / 1e6
+                file = open("data/" + filename, "r")
+                line = file.readlines()[0]
+                data_match = re.match("(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+)",
+                                      line)
+                if data_match:
+                    timestep_data = np.append(timestep_data, [[time, data_match.group(1), data_match.group(4)]], axis=0)
+        timestep_data = timestep_data[timestep_data[:, 0].argsort()]  # Sort by time
+        data.append(timestep_data)
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(times, positions, 'b')
+    for timestep_data in data:
+        ax1.plot(timestep_data[:, 0], timestep_data[:, 1], 'b--')
+    ax1.set_xlabel('time (s)')
+    # Make the y-axis label, ticks and tick labels match the line color.
+    ax1.set_ylabel('Position', color='b')
+    # ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(times, velocities, 'r')
+    for timestep_data in data:
+        ax2.plot(timestep_data[:, 0], timestep_data[:, 2], 'r--')
+    ax2.set_ylabel('Velocity', color='r')
+    # ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+    plt.show()
+
+
+plot_pos_and_vel()
---
+++ verification/friction/friction_sim.c
@@ -0,0 +1,104 @@
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+
+#include <CL/cl.h>
+
+#endif
+
+#include <stdio.h>
+#include "../../util/clUtils/clUtils.h"
+#include "../../util/particleUtils/particleUtils.h"
+#include "../../tests/run_tests/run_tests.h"
+#include "../../util/simUtils/simUtils.h"
+#include "../../sims/simRunner/simRunner.h"
+#include "../../util/wallUtils/wallUtils.h"
+#include "../../structures/wall.h"
+#include <math.h>
+
+#define MAX_SOURCE_SIZE (0x100000)
+#define VERBOSE FALSE
+#define LOG_DATA TRUE
+
+char prefix[50];
+
+particle *hparticles;
+cl_ulong NUMPART = 1;
+
+aa_wall *hwalls;
+cl_ulong NUMWALLS = 1;
+
+cl_float density = 2000;
+cl_float particle_diameter = 0.1;
+cl_float fluid_viscosity = 0.0000193 * 100;
+
+cl_float stiffness = 1e5;
+cl_float restitution_coefficient = 0.8;
+cl_float friction_coefficient = 0.6;
+cl_float friction_stiffness = 1e9;
+cl_float cohesion_stiffness = 0;
+
+cl_float initial_vel_x = 1;
+
+cl_float timestep;
+cl_float sim_length = 2.5;
+cl_float log_step = 0.03125;
+
+cl_float domain_length;
+
+cl_context context;
+cl_device_id device;
+
+int main() {
+
+    // Initializing OpenCL.
+    setContext(&device, &context, TRUE);
+
+    // Run tests
+    if (!run_all_tests(device, context, FALSE)) {
+        return 1;
+    }
+
+    char *iterate_particle_files[] = {"../util/kernelUtils.cl",
+                                      "../kernels/get_vel_fluid/no_drag.cl",
+                                      "../kernels/iterate_particle.cl"};
+    cl_kernel iterate_particle = getKernel(device, context, iterate_particle_files, 3, "iterate_particle", TRUE);
+
+    hparticles = malloc(sizeof(particle) * NUMPART);
+
+    hparticles[0].diameter = particle_diameter;
+    hparticles[0].effect_diameter = 0;
+    hparticles[0].fluid_viscosity = fluid_viscosity;
+    hparticles[0].density = density;
+    hparticles[0].id = 0;
+    hparticles[0].pos = (cl_float3) {0, 0, 0};
+    hparticles[0].vel = (cl_float3) {initial_vel_x, 0, 0};
+    hparticles[0].forces = (cl_float3) {0, 0, 0};
+
+    hwalls = malloc(sizeof(aa_wall) * NUMWALLS);
+    domain_length = 1;
+    hwalls[0].min = (cl_float3) {-domain_length / 2, 0, -domain_length / 2};
+    hwalls[0].max = (cl_float3) {domain_length / 2, 0, domain_length / 2};
+    hwalls[0].normal = get_aa_wall_normal(hwalls[0]);
+
+    float collision_time = 3.14159 * sqrt(get_particle_mass(&(hparticles[0])) / stiffness);
+    printf("Mass = %f\n", get_particle_mass(&(hparticles[0])));
+    printf("Collision Duration = %f\n", collision_time);
+
+    float theoretical_overlap = get_particle_mass(&(hparticles[0])) * 9.81 / stiffness;
+    printf("Theoretical Overlap = %f\n", theoretical_overlap);
+
+    char dir[] = "../verification/friction/data";
+    for (int i = 32; i <= 80; i += 16) {
+        timestep = collision_time / i;
+
+        sprintf(prefix, "friction_%i", i);
+
+        hparticles[0].pos = (cl_float3) {0, particle_diameter / 2 - theoretical_overlap, 0};
+        hparticles[0].vel = (cl_float3) {initial_vel_x, 0, 0};
+
+        runSim(hparticles, NUMPART, iterate_particle, particle_diameter, hwalls, NUMWALLS, stiffness,
+               restitution_coefficient, friction_coefficient, stiffness, cohesion_stiffness, domain_length,
+               prefix, dir, sim_length, timestep, VERBOSE, LOG_DATA, TRUE, log_step, device, context);
+    }
+}
